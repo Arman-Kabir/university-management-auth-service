d7f13ebaaa0335271ee2aa12f05b9a01d974d838
Arman-Kabir <armankabir8@gmail.com>
2023-06-01 12:41:19 +0600

    UM-7

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import usersRouter from './app/modules/users/users.route'
+import usersService from './app/modules/users/users.service'
 
 const app: Application = express()
 // const port = 3000;
@@ -10,7 +12,16 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: Response) => {
+// Appplication routes
+app.use('api/v1/users/', usersRouter)
+
+app.get('/', async (req: Request, res: Response) => {
+  await usersService.createUser({
+    id: '999',
+    password: '1234',
+    role: 'student',
+  })
+
   res.send('working successfully')
 })
 
