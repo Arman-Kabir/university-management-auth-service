57787a61c93b8cdfa6279d85bbacc20baaf13f0d
Arman-Kabir <armankabir8@gmail.com>
2023-06-05 09:00:12 +0600

    few refactor

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
-import express, { Application } from 'express'
+import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 import usersRouter from './app/modules/users/users.route'
-import globalErrorHandler from './app/middlewares/globalErrorHandler'
+// import globalErrorHandler from './app/middlewares/globalErrorHandler'
 // import usersService from './app/modules/users/users.service'
 
 const app: Application = express()
@@ -16,21 +16,21 @@ app.use(express.urlencoded({ extended: true }))
 // Appplication routes
 app.use('api/v1/users/', usersRouter)
 
-// app.get('/', (req: Request, res: Response,next:NextFunction) => {
-//   // await usersService.createUser({
-//   //   id: '999',
-//   //   password: '1234',
-//   //   role: 'student',
-//   // })
+app.get('/', async (req: Request, res: Response) => {
+  // await usersService.createUser({
+  //   id: '999',
+  //   password: '1234',
+  //   role: 'student',
+  // })
 
-//   // res.send('working successfully')
+  res.send('working successfully')
 
-//   throw new Error('ore error');
-//   // throw new ApiError(400,'ore error')
-//   // next('ore error');
-// })
+  // throw new Error('ore error');
+  // throw new ApiError(400,'ore error')
+  // next('ore error');
+})
 
 //global error handler
-app.use(globalErrorHandler)
+// app.use(globalErrorHandler)
 
 export default app
