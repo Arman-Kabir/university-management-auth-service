1895919a7f149e6f15181192f77c51a1fecb9fd8
Arman-Kabir <armankabir8@gmail.com>
2023-06-08 09:51:29 +0600

    zod error done

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,10 +1,12 @@
 /* eslint-disable no-unused-expressions */
-import { ErrorRequestHandler } from 'express'
-import { IGenericErrorMessage } from '../../interfaces/error'
-import ApiError from '../../errors/ApiError'
-import handleValidationError from '../../errors/handleValidationError'
-import config from '../../config'
-import { errorlogger } from '../../shared/logger'
+import { ErrorRequestHandler } from 'express';
+import { IGenericErrorMessage } from '../../interfaces/error';
+import ApiError from '../../errors/ApiError';
+import handleValidationError from '../../errors/handleValidationError';
+import config from '../../config';
+import { errorlogger } from '../../shared/logger';
+import handleZodError from '../../errors/handleZOdError';
+import { ZodError } from 'zod';
 
 const globalErrorHandler: ErrorRequestHandler = (
   // err,
@@ -22,21 +24,26 @@ const globalErrorHandler: ErrorRequestHandler = (
 
   config.env === 'development'
     ? console.log('global error handler', error)
-    : errorlogger.error('global error handler', error)
+    : errorlogger.error('global error handler', error);
 
-  let statusCode = 500
-  let message = 'Something went wrong'
-  let errorMessages: IGenericErrorMessage[] = []
+  let statusCode = 500;
+  let message = 'Something went wrong';
+  let errorMessages: IGenericErrorMessage[] = [];
 
   if (error?.name === 'ValidationError') {
-    const simplifiedError = handleValidationError(error)
+    const simplifiedError = handleValidationError(error);
 
-    statusCode = simplifiedError.statusCode
-    message = simplifiedError.message
-    errorMessages = simplifiedError.errorMessages
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessages = simplifiedError.errorMessages;
+  } else if (error instanceof ZodError) {
+    const simplifiedError = handleZodError(error);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessages = simplifiedError.errorMessages;
   } else if (error instanceof ApiError) {
-    statusCode = error?.statusCode
-    message = error.message
+    statusCode = error?.statusCode;
+    message = error.message;
     errorMessages = error?.message
       ? [
           {
@@ -44,9 +51,9 @@ const globalErrorHandler: ErrorRequestHandler = (
             message: error?.message,
           },
         ]
-      : []
+      : [];
   } else if (error instanceof Error) {
-    message = error?.message
+    message = error?.message;
     errorMessages = error?.message
       ? [
           {
@@ -54,7 +61,7 @@ const globalErrorHandler: ErrorRequestHandler = (
             message: error?.message,
           },
         ]
-      : []
+      : [];
   }
 
   res.status(statusCode).json({
@@ -62,12 +69,12 @@ const globalErrorHandler: ErrorRequestHandler = (
     message,
     errorMessages,
     // stack: config.env !== 'production' ? error?.stack:undefined ,
-  })
+  });
 
-  next()
-}
+  next();
+};
 
-export default globalErrorHandler
+export default globalErrorHandler;
 
 //path:
 //message:
--- src/errors/handleValidationError.ts
+++ src/errors/handleValidationError.ts
@@ -1,26 +1,26 @@
-import mongoose from 'mongoose'
-import { IGenericErrorResponse } from '../interfaces/common'
-import { IGenericErrorMessage } from '../interfaces/error'
+import mongoose from 'mongoose';
+import { IGenericErrorResponse } from '../interfaces/common';
+import { IGenericErrorMessage } from '../interfaces/error';
 
 const handleValidationError = (
-  err: mongoose.Error.ValidationError
+  error: mongoose.Error.ValidationError
 ): IGenericErrorResponse => {
-  const errors: IGenericErrorMessage[] = Object.values(err.errors).map(
+  const errors: IGenericErrorMessage[] = Object.values(error.errors).map(
     (el: mongoose.Error.ValidationError | mongoose.Error.CastError) => {
       return {
         path: el?.path,
         message: el?.message,
-      }
+      };
     }
-  )
+  );
 
-  const statusCode = 400
+  const statusCode = 400;
   return {
     statusCode,
     message: 'Validation Error',
     errorMessages: errors,
-  }
+  };
   //Rest of the code
-}
+};
 
-export default handleValidationError
+export default handleValidationError;
