b284794d01e92df91324a2009051cda6e4112637
Arman-Kabir <armankabir8@gmail.com>
2023-06-10 05:32:53 +0600

    same year same semester done

--- src/app/modules/academicSemester/academicSemesterModel.ts
+++ src/app/modules/academicSemester/academicSemesterModel.ts
@@ -8,6 +8,8 @@ import {
   academicSemesterMonths,
   academicSemesterTitles,
 } from './academicSemester.constant';
+import ApiError from '../../../errors/ApiError';
+import status from 'http-status';
 
 const academicSemesterSchema = new Schema<IAcademicSemester>(
   {
@@ -40,6 +42,21 @@ const academicSemesterSchema = new Schema<IAcademicSemester>(
     timestamps: true,
   }
 );
+
+//handling same year and same semester issue
+//Data - > check -? Same Year && same semester
+
+academicSemesterSchema.pre('save', async function (next) {
+  const isExist = await AcademicSemester.findOne({
+    title: this.title,
+    year: this.year,
+  });
+  if (isExist) {
+    throw new ApiError(status.CONFLICT, 'Academic semester is already exist');
+  }
+  //next is mongoose hook
+  next();
+});
 
 export const AcademicSemester = model<IAcademicSemester, AcademicSemesterModel>(
   'AcademicSemester',
