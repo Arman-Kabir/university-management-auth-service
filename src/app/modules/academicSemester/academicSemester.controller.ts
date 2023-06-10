98226db9b3cf66613362d0b30d4f751117270c01
Arman-Kabir <armankabir8@gmail.com>
2023-06-10 18:49:57 +0600

    optimized using higher order function

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -1,23 +1,39 @@
-import { RequestHandler } from 'express';
+import { Request, Response } from 'express';
 import { AcademicSemesterService } from './academicSemester.service';
+import catchAsync from '../../../shared/catchAsync';
 
-const createSemester: RequestHandler = async (req, res, next) => {
-  try {
-    const { ...academicSemesterData } = req.body;
-    const result = await AcademicSemesterService.createSemester(
-      academicSemesterData
-    );
-    // logger('hello');
-    res.status(200).json({
-      success: true,
-      message: 'academicSemester created successfully',
-      data: result,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+const createSemester = catchAsync(async (req: Request, res: Response) => {
+  const { ...academicSemesterData } = req.body;
+
+  const result = await AcademicSemesterService.createSemester(
+    academicSemesterData
+  );
+  // logger('hello');
+  res.status(200).json({
+    success: true,
+    message: 'academicSemester created successfully',
+    data: result,
+  });
+});
 
 export const academicSemesterController = {
   createSemester,
 };
+
+//code before optimization
+// async (req, res, next) => {
+//   try {
+//     const { ...academicSemesterData } = req.body;
+//     const result = await AcademicSemesterService.createSemester(
+//       academicSemesterData
+//     );
+//     // logger('hello');
+//     res.status(200).json({
+//       success: true,
+//       message: 'academicSemester created successfully',
+//       data: result,
+//     });
+//   } catch (err) {
+//     next(err);
+//   }
+// }
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,24 +1,46 @@
-import { RequestHandler } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { UserService } from './user.service';
+import catchAsync from '../../../shared/catchAsync';
 
-const createUser: RequestHandler = async (req, res, next) => {
-  try {
-    const { user } = req.body;
-    const result = await UserService.createUser(user);
-    // logger('hello');
-    res.status(200).json({
-      success: true,
-      message: 'user created successfully',
-      data: result,
-    });
-  } catch (err) {
-    // res.status(400).json({
-    //   error:err
-    // })
-    next(err);
+const createUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { user } = req.body;
+      const result = await UserService.createUser(user);
+      // logger('hello');
+      res.status(200).json({
+        success: true,
+        message: 'user created successfully',
+        data: result,
+      });
+    } catch (err) {
+      // res.status(400).json({
+      //   error:err
+      // })
+      next(err);
+    }
   }
-};
+);
 
 export const UserController = {
   createUser,
 };
+
+//code before optimization
+// async (req, res, next) => {
+//   try {
+//     const { user } = req.body;
+//     const result = await UserService.createUser(user);
+//     // logger('hello');
+//     res.status(200).json({
+//       success: true,
+//       message: 'user created successfully',
+//       data: result,
+//     });
+//   } catch (err) {
+//     // res.status(400).json({
+//     //   error:err
+//     // })
+//     next(err);
+//   }
+// };
