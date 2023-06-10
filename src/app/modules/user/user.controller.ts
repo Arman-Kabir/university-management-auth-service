465d7b05517311ea585ce72dde6866bef7265bb7
Arman-Kabir <armankabir8@gmail.com>
2023-06-10 21:03:07 +0600

    try catch optimized

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,24 +1,34 @@
 import { NextFunction, Request, Response } from 'express';
 import { UserService } from './user.service';
 import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import httpStatus from 'http-status';
 
 const createUser = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const { user } = req.body;
-      const result = await UserService.createUser(user);
-      // logger('hello');
-      res.status(200).json({
-        success: true,
-        message: 'user created successfully',
-        data: result,
-      });
-    } catch (err) {
-      // res.status(400).json({
-      //   error:err
-      // })
-      next(err);
-    }
+    // try {
+    const { user } = req.body;
+    const result = await UserService.createUser(user);
+    // logger('hello');
+
+    next();
+    // res.status(200).json({
+    //   success: true,
+    //   message: 'user created successfully',
+    //   data: result,
+    // });
+    // } catch (err) {
+    //   // res.status(400).json({
+    //   //   error:err
+    //   // })
+    //   next(err);
+    // }
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'User Created Successfully',
+      data: result,
+    });
   }
 );
 
